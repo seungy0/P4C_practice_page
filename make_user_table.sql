3b883ec65c75bb40b4a0648ab5d1ecffc10d9e6a
seungy0 <dltmdrb98@gmail.com>
2022-09-22 06:39:27 +0000

    ADD sql files

---
+++ make_user_table.sql
@@ -0,0 +1,10 @@
+use forum;
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+    `useridx` int primary key auto_increment,
+    `userid` varchar(255) unique not null,
+    `userpw` varchar(255) not null,
+    `username` varchar(255) not null,
+    `useremail` varchar(255),
+    `regdate` datetime DEFAULT now()
+);
